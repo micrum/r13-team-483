6a95a5e4b4a41fd01b051d5b8c0d3939cc76ea80
stas <s.slunkov@gmail.com>
2013-10-19 17:43:49 +0300

    real numbers are now calculated for sys/user/real time columns

--- app/services/performator.rb
+++ app/services/performator.rb
@@ -1,18 +1,64 @@
+require 'erb'
+require 'tempfile'
+require 'rbconfig'
+require 'yaml'
+
 class Performator
+  RUBY = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
+
   def run(sample_group)
     sample_group.samples.each do |sample|
-      run_sample sample
+      run_sample(sample)
     end
   end
 
   private
 
   def run_sample(sample)
-    sample.sys_time = rand(100)/100.0
-    sample.user_time = rand(100)/100.0
-    sample.real_time = rand(100)/100.0
-    sample.memory = rand(100.megabytes) + 10.megabytes
-    sample.iterations_count = rand(10_000)
+    file = create_bench_file(sample)
+    results = run_bench_file(file)
+
+    sample.sys_time = results.stime
+    sample.user_time = results.utime
+    sample.real_time = results.real
+
+    sample.memory = 0
+    sample.iterations_count = 0
     sample.save!
   end
+
+  def create_bench_file(sample)
+    bench_template_path = Rails.root.join('app/assets/templates/bench_file.erb')
+    bench_file = Tempfile.new('bench_file')
+    parsed_code = parse_code(sample.code)
+    init = parsed_code[:init]
+    bench = parsed_code[:bench]
+    erb(bench_template_path, bench_file.path, binding)
+
+    bench_file
+  end
+
+  def run_bench_file(file)
+    cmd = %Q<#{RUBY} "#{file.path}">
+
+    results = nil
+    IO.popen(cmd, 'w+') do |io|
+      output = io.read
+      results = YAML.load(output) rescue nil
+    end
+
+    results
+  end
+
+  def parse_code(code)
+    h = {}
+    h[:init], h[:bench] = code.gsub('(do not delete the comment)', '').split('# benchmark')
+    h
+  end
+
+  def erb template_file_path, output_file_path, binding
+    contents = ERB.new(File.read(template_file_path), nil, '-').result binding
+    File.write(output_file_path, contents)
+  end
+
 end
--- spec/services/perfomator_spec.rb
+++ spec/services/perfomator_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Performator do
+  let(:perf) { Performator.new }
 
   describe '#run' do
-    let(:perf) { Performator.new }
+
     let(:sample_group) { Fabricate(:sample_group) }
 
     it 'stores something in columns' do
@@ -17,6 +18,35 @@ describe Performator do
         expect(sample.real_time).to be
         expect(sample.iterations_count).to be
       end
+    end
+
+  end
+
+  describe '#create_bench_file' do
+    let(:sample) { Fabricate(:sample) }
+
+    it 'creates bench_file' do
+      bench_file = perf.send(:create_bench_file, sample)
+      expect(bench_file.read).not_to be_empty
+    end
+
+    it 'blocks in bench_file replaced with code from sample' do
+      bench_file = perf.send(:create_bench_file, sample)
+      contents = bench_file.read
+      expect(contents).to include('arr.size')
+      expect(contents).to include('arr = [0] * 1_000')
+    end
+
+  end
+
+  describe '#run_sample' do
+    let(:sample) { Fabricate(:sample) }
+
+    it 'updated sys_time, user_time, real_time columns' do
+      perf.send(:run_sample, sample)
+      expect(sample.sys_time).to be >= 0
+      expect(sample.user_time).to be >= 0
+      expect(sample.real_time).to be > 0
     end
   end
 
