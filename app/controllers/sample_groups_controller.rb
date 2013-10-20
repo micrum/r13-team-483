5a2399fc6d2f13d035f8c825a80a94a31602b039
stas <s.slunkov@gmail.com>
2013-10-20 14:11:45 +0300

    move samples initialization

--- app/controllers/sample_groups_controller.rb
+++ app/controllers/sample_groups_controller.rb
@@ -14,7 +14,17 @@ class SampleGroupsController < ApplicationController
 
   def new
     @sample_group = SampleGroup.new
-    @sample_group.samples.build
+    sample = @sample_group.samples.build
+    sample.code = <<CODE
+# init (do not delete the comment)
+arr = [0] * 1_000
+
+# benchmark (do not delete the comment)
+1000000.times do
+  arr.size
+end
+CODE
+
   end
 
 
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,25 +1,10 @@
 class Sample < ActiveRecord::Base
   belongs_to :sample_group
-  after_initialize :set_defaults
 
   validates_presence_of :code
   before_save :validate_code
 
   private
-
-  def set_defaults
-    if new_record?
-      self.code = <<CODE
-# init (do not delete the comment)
-arr = [0] * 1_000
-
-# benchmark (do not delete the comment)
-1000000.times do
-  arr.size
-end
-CODE
-    end
-  end
 
   def validate_code
     policy = RubyCop::Policy.new
