572d178247b6b1c3fa69eeb1eb8b603ea8b95d8c
Basil Gren <bas.gren@gmail.com>
2013-10-21 02:27:32 +0300

    Fix routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 RubyBench::Application.routes.draw do
 
-  resources :samples
-
-
   # Small hack for creating benchmark by some template
-  post 'benchmark/new' => 'sample_groups#new'
+  post 'benchmarks/new' => 'sample_groups#new'
 
   resources :sample_groups, path: 'benchmarks' do
     get 'results', :to => 'sample_groups#results'
